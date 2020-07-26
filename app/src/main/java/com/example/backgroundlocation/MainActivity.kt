19a34d560dd116f077017124830d43e2d25457ea
shinji ichien <shinji11088011@gmail.com>
2020-07-26 22:11:13 +0900

    use permission-dispatcher

--- app/src/main/java/com/example/backgroundlocation/MainActivity.kt
+++ app/src/main/java/com/example/backgroundlocation/MainActivity.kt
@@ -5,24 +5,25 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.app.ActivityCompat
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import permissions.dispatcher.NeedsPermission
+import permissions.dispatcher.RuntimePermissions
 
 
+@RuntimePermissions
 class MainActivity : AppCompatActivity() {
     companion object {
-        private const val PERMISSION_REQUEST_CODE = 1234
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        requestPermission()
+        requestPermissionWithPermissionCheck();
         createNotificationChannel()
 
         startButton.setOnClickListener {
@@ -36,39 +37,12 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun requestPermission() {
-        val permissionAccessCoarseLocationApproved =
-                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) ==
-                        PackageManager.PERMISSION_GRANTED &&
-                        ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
-                        PackageManager.PERMISSION_GRANTED
+    @NeedsPermission(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+    fun requestPermission() {
+        Log.d("loglog", "位置情報権限申請")
 
-        if (permissionAccessCoarseLocationApproved) {
-            val backgroundLocationPermissionApproved = ActivityCompat
-                    .checkSelfPermission(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION) ==
-                    PackageManager.PERMISSION_GRANTED
-
-            if (backgroundLocationPermissionApproved) {
-                // フォアグラウンドとバックグランドのバーミッションがある
-            } else {
-                // フォアグラウンドのみOKなので、バックグラウンドの許可を求める
-                ActivityCompat.requestPermissions(this,
-                        arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION),
-                        PERMISSION_REQUEST_CODE
-                )
-            }
-        } else {
-            // 位置情報の権限が無いため、許可を求める
-            ActivityCompat.requestPermissions(this,
-                    arrayOf(
-                            Manifest.permission.ACCESS_COARSE_LOCATION,
-                            Manifest.permission.ACCESS_FINE_LOCATION,
-                            Manifest.permission.ACCESS_BACKGROUND_LOCATION
-                    ),
-                    PERMISSION_REQUEST_CODE
-            )
-        }
     }
+
     private fun createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(
